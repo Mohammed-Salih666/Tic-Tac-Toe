1363b1491fd9a0657413d99a491c3888fb465f44
Mohammed-Salih666 <mohammedsaleh0218@gmail.com>
2023-06-24 18:58:11 +0300

    Program the checkWinner function

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ const Player = (name, marker) => {
 };
 
 
-
-
 const gameBoard = (() => {
     const board = ["", "", "","","","","","",""]; 
     const boxes = document.querySelectorAll('.box');
@@ -23,6 +21,22 @@ const gameBoard = (() => {
         }
     }
 
+    const checkWinner = () => {
+        let winner = 
+        board[0] === board[1] && board[1] === board[2] ? board[0] 
+        : board[3] === board[4] && board[4] === board[5] ? board[3]
+        : board[6] === board[7] && board[7] === board[8] ? board[6]
+
+        : board[0] === board[3] && board[3] === board[6] ? board[0]
+        : board[1] === board[4] && board[4] === board[7] ? board[1]
+        : board[2] === board[5] && board[5] === board[8] ? board[2]
+
+        : board[0] === board[4] && board[4] === board[8] ? board[0]
+        : board[2] === board[4] && board[4] === board[6] ? board[2]
+        : undefined;
+
+        return winner; 
+    }
 
     startButton.addEventListener("click", init);
     
@@ -32,14 +46,29 @@ const gameBoard = (() => {
 
     const play = (index) => {
 
-        board[index] = currentPlayer.marker; 
-        boxes[index].textContent = currentPlayer.marker;
+        if(currentPlayer != null) {
+            board[index] = currentPlayer.marker; 
+            boxes[index].textContent = currentPlayer.marker;
+        }
+  
 
 
         player1 == currentPlayer ? currentPlayer = player2
         : currentPlayer = player1;
 
-       
+        let winner = checkWinner();
+        console.log(winner);
+        if(winner !== undefined && winner != "") {
+            const winnerDiv = document.createElement('div');
+            winnerDiv.id = "winner"; 
+            winnerDiv.textContent = winner === "X"? player1 : player2; 
+            document.body.appendChild(winnerDiv);
+            currentPlayer = null;
+        }
+        if(!board.includes("")) {
+
+        }
+
     };
 
 
